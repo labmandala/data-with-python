7f2fb0ada1e6f59e40d300d8962ce22debdebd12
sarah <skg@neofilm.us>
2025-05-02 20:37:15 -0400

    Analyze a dataset to compare two groups

--- tips-and-group-size/main.py
+++ tips-and-group-size/main.py
@@ -6,3 +6,12 @@ with open("tips.csv", "r") as file:
   headers = next(data)
   data_list = list(data)
   data_numpy = np.array(data_list)
+
+size = data_numpy[:,6]
+# print(size)
+tips = np.array(data_numpy[:,1], dtype=float)
+# print(tips)
+bills = np.array(data_numpy[:,0], dtype=float)
+# print(bills)
+tip_percentages = tips/bills
+# print(tip_percentages)
