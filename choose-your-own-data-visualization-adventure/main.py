08aa675aecbfe704d6cfa78619d0c546c1e01dc7
sarah <skg@neofilm.us>
2025-08-15 08:55:38 -0400

    Create visualization to display data relationship for Kaggle books dataset

--- choose-your-own-data-visualization-adventure/main.py
+++ choose-your-own-data-visualization-adventure/main.py
@@ -5,4 +5,9 @@ import matplotlib.pyplot as plt
 with open("books.csv","r") as datafile:
     data = pd.read_csv(datafile,delimiter=",")
 
-print(data["language_code"])
+sns.scatterplot(x="average_rating", y="# num_pages", size="# num_pages", sizes=(20, 180), data=data)
+
+plt.xlabel("Average Rating")
+plt.ylabel("Number of Pages")
+plt.title("Average Rating and Number of Pages")
+plt.savefig("Ratings-and-Pages.png")
